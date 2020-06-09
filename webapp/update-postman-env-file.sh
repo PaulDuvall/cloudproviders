d7f79e8a5066175393bbcc25ff368a07ad667eaf
“Paul <pduvall@gmail.com>
2020-06-09 13:02:44 +0000

    add API Tests to CodeBuild and Pipeline

---
+++ webapp/update-postman-env-file.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#This shell script updates Postman environment file with the API Gateway URL created
+# via the api gateway deployment
+
+echo "Running update-postman-env-file.sh"
+
+SAM_STACK_NAME=${1:-junk}   
+
+echo "SAM_STACK_NAME is $SAM_STACK_NAME"
+
+# Get the 3rd Output value in the CloudFormation stack
+api_gateway_url=`aws cloudformation describe-stacks \
+  --stack-name '$SAM_STACK_NAME' \
+  --query "Stacks[0].Outputs[3].{OutputValueValue:OutputValue}" --output text`
+
+echo "API Gateway URL:" ${api_gateway_url}
+
+jq -e --arg apigwurl "$api_gateway_url" '(.values[] | select(.key=="apigw-root") | .value) = $apigwurl' \
+  postman_environment.json > postman_environment.json.tmp \
+  && cp postman_environment.json.tmp postman_environment.json \
+  && rm postman_environment.json.tmp
+
+echo "Updated postman_environment.json"
+
+cat postman_environment.json
