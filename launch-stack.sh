6d1d377d7003780a1940944e293b5a6ce8bcaffc
“Paul <pduvall@gmail.com>
2020-06-09 13:13:46 +0000

    change order of s3 cp so that it is in the correct dir

--- launch-stack.sh
+++ launch-stack.sh
@@ -21,13 +21,6 @@ aws cloudformation wait stack-delete-complete --stack-name pmd-serverless-app
 
 cd cloudproviders/webapp
 
-aws s3 mb s3://pmd-serverless-app-$(aws sts get-caller-identity --output text --query 'Account')
-zip -r pmd-serverless-app.zip .
-mkdir zipfiles
-cp pipeline.yml zipfiles
-mv pmd-serverless-app.zip zipfiles
-cd zipfiles
-
 aws s3 sync . s3://pmd-serverless-app-$(aws sts get-caller-identity --output text --query 'Account')
 
 aws s3 cp collection.json \
@@ -35,5 +28,12 @@ s3://pmd-serverless-app-$(aws sts get-caller-identity --output text --query 'Acc
 
 aws s3 cp postman_environment.json \
 s3://pmd-serverless-app-$(aws sts get-caller-identity --output text --query 'Account')/postman-env-files/postman_environment.json
+
+aws s3 mb s3://pmd-serverless-app-$(aws sts get-caller-identity --output text --query 'Account')
+zip -r pmd-serverless-app.zip .
+mkdir zipfiles
+cp pipeline.yml zipfiles
+mv pmd-serverless-app.zip zipfiles
+cd zipfiles
 
 aws cloudformation create-stack --stack-name pmd-serverless-app --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://pipeline.yml --parameters ParameterKey=CodeCommitS3Bucket,ParameterValue=pmd-serverless-app-$(aws sts get-caller-identity --output text --query 'Account') ParameterKey=CodeCommitS3Key,ParameterValue=pmd-serverless-app.zip
