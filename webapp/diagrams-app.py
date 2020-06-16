a75a16fc1eddee8cdfc265fb24df1e2b8ee3cf70
“Paul <pduvall@gmail.com>
2020-06-16 14:44:03 +0000

    modify relationships to better reflect reality

--- webapp/diagrams-app.py
+++ webapp/diagrams-app.py
@@ -17,15 +17,21 @@ with Diagram("Serverless Web Apps", show=False, direction="TB"):
 
     with Cluster("CloudFormation"):
         cloudformation = Cloudformation("Stack")
-        cloudformation >> IdentityAndAccessManagementIam("IAM") >> Codecommit("CodeCommit") >> Codebuild("CodeBuild") >> S3("S3") >> Codepipeline("CodePipeline")
+        cloudformation >> IdentityAndAccessManagementIam("IAM")
+        cloudformation >> Codecommit("CodeCommit")
+        cloudformation >> Codebuild("CodeBuild")
+        cloudformation >> Codepipeline("CodePipeline")
+        cloudformation >> S3("S3")
 
     with Cluster("CodePipeline"):
         codepipeline = Codepipeline("Pipeline")
-        codepipeline >> Codecommit("CodeCommit") >> Codebuild("CodeBuild") >> Cloudformation("CloudFormation")
-      
+        codepipeline >> Codecommit("CodeCommit")
+        codepipeline >> Codebuild("CodeBuild")
+        codepipeline >> Cloudformation("CloudFormation")
+        
     with Cluster("Serverless Application Model"):
         sam = Cloudformation("SAM Template")
-        sam >> APIGateway("API Gateway") >> Lambda("Lambda") >> DynamodbTable("DynamoDB")
+        sam >> APIGateway("API Gateway")
+        sam >> Lambda("Lambda")
+        sam >> DynamodbTable("DynamoDB")     
         cloudformation >> codepipeline >> sam
-        
-
