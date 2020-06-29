a0e0b89780a7a631e0eacd1f231ac1160c12a24f
“Paul <pduvall@gmail.com>
2020-06-29 15:39:57 +0000

    pass function version

--- webapp/src/PreTraffic.js
+++ webapp/src/PreTraffic.js
@@ -1,44 +1,54 @@
 // @ts-check
 'use strict';
 
+// From https://github.com/singledigit/sessions-with-sam/blob/master/safe-deploy/src/hooks/basepre.js
+
 const aws = require('aws-sdk');
-const codedeploy = new aws.CodeDeploy({apiVersion: '2014-10-06'});
+const codedeploy = new aws.CodeDeploy();
+const lambda = new aws.Lambda();
 
-exports.handler = async (event, context, callback) => {
+exports.lambdaHandler = async (event) => {
+  let status = 'Failed';
 
-    console.log("Entering PreTraffic Hook!");
-    console.log(JSON.stringify(event));
+  console.log("Entering PreTraffic Hook!");
+  console.log(JSON.stringify(event));
 
-    //Read the DeploymentId from the event payload.
-    let deploymentId = event.DeploymentId;
-    console.log("deploymentId=" + deploymentId);
+  //Read the DeploymentId from the event payload.
+  let deploymentId = event.DeploymentId;
+  console.log("deploymentId=" + deploymentId);
 
-    //Read the LifecycleEventHookExecutionId from the event payload
-    let lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
-    console.log("lifecycleEventHookExecutionId=" + lifecycleEventHookExecutionId);
+  //Read the LifecycleEventHookExecutionId from the event payload
+  let lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
+  console.log("lifecycleEventHookExecutionId=" + lifecycleEventHookExecutionId);
 
-    /*
-      [Perform validation or prewarming steps here]
-    */
 
-    // Prepare the validation test results with the deploymentId and
-    // the lifecycleEventHookExecutionId for AWS CodeDeploy.
-    let params = {
-        deploymentId: deploymentId,
-        lifecycleEventHookExecutionId: lifecycleEventHookExecutionId,
-        status: 'Succeeded' // status can be 'Succeeded' or 'Failed'
-    };
+  let invokeParams = {
+    FunctionName: process.env.FUNCTION_VERSION
+  }
 
-    try {
-      await codedeploy.putLifecycleEventHookExecutionStatus(params).promise();
-      console.log("putLifecycleEventHookExecutionStatus done. executionStatus=[" + params.status + "]");
-      return 'Validation test succeeded'
-    }
-    catch (err) {
-      console.log("putLifecycleEventHookExecutionStatus ERROR: " + err);
-      throw new Error('Validation test failed')
-    }
+  try{
+    let returnEvent = await lambda.invoke(invokeParams).promise()
+    console.log(returnEvent);
+    console.log(JSON.parse(returnEvent.Payload))
+    if(JSON.parse(returnEvent.Payload).preTest) status = 'Succeeded';
+  } catch (err) {
+    console.log('error invoking Lambda');
+    console.log(err)
+  }
 
-}
+  // Prepare the validation test results with the deploymentId and
+  // the lifecycleEventHookExecutionId for AWS CodeDeploy.
+  let params = {
+    deploymentId: deploymentId,
+    lifecycleEventHookExecutionId: lifecycleEventHookExecutionId,
+    status: status // status can be 'Succeeded' or 'Failed'
+  };
 
-// See more: https://docs.aws.amazon.com/codedeploy/latest/userguide/reference-appspec-file-structure-hooks.html#reference-appspec-file-structure-hooks-section-structure-lambda-sample-function
+  try {
+    await codedeploy.putLifecycleEventHookExecutionStatus(params).promise();
+    console.log("putLifecycleEventHookExecutionStatus done. executionStatus=[" + params.status + "]");
+    return 'Validation test succeeded'
+  } catch (err) {
+    console.log("putLifecycleEventHookExecutionStatus ERROR: " + err);
+    throw new Error('Validation test failed')
+  
