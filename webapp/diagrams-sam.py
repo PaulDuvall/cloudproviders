6ec6f2395f1c168f7fd55421603eb6d52a049e57
“Paul <pduvall@gmail.com>
2020-07-01 20:45:47 +0000

    expand canary deployment diagram

--- webapp/diagrams-sam.py
+++ webapp/diagrams-sam.py
@@ -1,5 +1,6 @@
 # https://diagrams.mingrammer.com/docs/nodes/aws
-from diagrams import Cluster, Diagram
+# https://www.graphviz.org/doc/info/attrs.html
+from diagrams import Cluster, Diagram, Edge
 from diagrams.aws.compute import Lambda
 from diagrams.aws.storage import S3
 from diagrams.aws.network import APIGateway
@@ -15,28 +16,50 @@ from diagrams.aws.general import User
 from diagrams.aws.general import General
 from diagrams.onprem.client import Client
 
-with Diagram("Gradual Deployments", show=False, direction="LR"):
+graph_attr = {
+    "fontsize": "45",
+    "bgcolor": "transparent",
+    "fillcolor": "red"
+}
 
+cluster_attr = {
+    "bgcolor": "lightgrey"
+}
 
-    cloudformation = Cloudformation("CloudFormation Stack")
+with Diagram("Canary Deployments", show=False, direction="LR", filename="canary-deployments", graph_attr=graph_attr):
 
+    with Cluster("Launch API URL", direction="LR"):
+        user = User("User")
+        console = Client("Browser")
+        user >> console
+
+    with Cluster("Cloud9", direction="LR"):
+        builder = User("Builder")
+        cli = CommandLineInterface("AWS CLI")
+        builder >> cli
+        
+    with Cluster("CloudFormation", direction="LR"):
+        cloudformation = Cloudformation("Stack")
+        cli >> cloudformation
 
     with Cluster("CodePipeline"):
         codepipeline = Codepipeline("Pipeline")
-        cfn  = Cloudformation("CloudFormation")
+        cfn  = Cloudformation("Deploy")
         codepipeline >> cfn
-        
-    with Cluster("Serverless Application Model"):
+
+    with Cluster("Serverless Application Model (SAM) Stack"):
         sam = Cloudformation("SAM Template")
         apigateway = APIGateway("API Gateway")
         mylambda = Lambda("Lambda")
         ddb  = DynamodbTable("DynamoDB")   
         codedeploy  = Codedeploy("CodeDeploy")
+        mylambda >> codedeploy
         sam >> apigateway
         sam >> mylambda
-        mylambda >> codedeploy
         sam >> ddb
-        
-        cfn >> sam
-        
+        mylambda >> ddb
+
         cloudformation >> codepipeline
+        cfn >> sam
+        console >> apigateway
+
