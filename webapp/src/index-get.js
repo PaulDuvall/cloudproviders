36f502dc9cd6d125df2e952ef0d6d278cada7f9c
EC2 Default User <ec2-user@ip-172-31-34-249.ec2.internal>
2020-07-02 14:54:28 +0000

    fail

--- webapp/src/index-get.js
+++ webapp/src/index-get.js
@@ -5,11 +5,11 @@ const docClient = new AWS.DynamoDB.DocumentClient({region: process.env.REGION});
 
 exports.handler = function(event, context, callback){
     console.log("Running index-get.js: " + context.functionName + ":" + context.functionVersion);
-    console.log('processing event: %j', event);
+    console.log('Version 2 processing event: %j', event);
 
     let scanningParameters = {
-        TableName: process.env.TABLE_NAME,
-        // TableName: "makeitfail14",
+        // TableName: process.env.TABLE_NAME,
+        TableName: "makeitfail14",
         Limit: 100 //maximum result of 100 items
     };
 
