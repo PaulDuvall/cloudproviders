7d4b1c4a86eecddde94421005b3e9efbc9888a7b
EC2 Default User <ec2-user@ip-172-31-34-249.ec2.internal>
2020-07-02 15:00:06 +0000

    add ec2 to dynamodb

--- webapp/src/index-get.js
+++ webapp/src/index-get.js
@@ -8,8 +8,8 @@ exports.handler = function(event, context, callback){
     console.log('Version 2 processing event: %j', event);
 
     let scanningParameters = {
-        // TableName: process.env.TABLE_NAME,
-        TableName: "makeitfail14",
+        TableName: process.env.TABLE_NAME,
+        // TableName: "makeitfail14",
         Limit: 100 //maximum result of 100 items
     };
 
