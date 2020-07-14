68f42077adb966f6cbf4c3dab1943de7da850e24
Paul Duvall <pduvall@gmail.com>
2020-07-14 18:58:55 +0000

    display static HTML

--- webapp/src/index-get.js
+++ webapp/src/index-get.js
@@ -2,6 +2,7 @@ console.log('Gets data from DynamoDB table')
 
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({region: process.env.REGION});
+const htmlResponse = require('./html-response');
 
 exports.handler = function(event, context, callback){
     console.log("Running index-get.js: " + context.functionName + ":" + context.functionVersion);
@@ -12,6 +13,23 @@ exports.handler = function(event, context, callback){
         // TableName: "makeitfail14",
         Limit: 100 //maximum result of 100 items
     };
+    
+    const thanksHtml = `
+      <html>
+      <head>
+        <meta charset="utf-8"/>
+      </head>
+      <body>
+        <h1>Thanks</h1>
+        <p>We received your submission</p>
+      </body>
+      </html>
+    `;
+
+    if (event.httpMethod === 'GET') {
+      return htmlResponse(thanksHtml);
+    }    
+    
 
     //In dynamoDB scan looks through your entire table and fetches all data
     docClient.scan(scanningParameters, function(err,data){
